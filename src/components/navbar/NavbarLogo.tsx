f2b01c6411f0dc9cddccca90fb6337ca726abc1d
Maryammmy <maryamsameh1610@gmail.com>
2025-12-12 15:05:14 +0200

    rawana white logo

--- src/components/navbar/NavbarLogo.tsx
+++ src/components/navbar/NavbarLogo.tsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
-// import Image from "../ui/Image";
+import Image from "../ui/Image";
 
 const NavbarLogo = () => {
   return (
     <Link to="/" className="flex flex-col items-center space-y-1">
-      <div className="w-20 sm:w-36 text-white text-3xl font-bold">
-        {/* <Image
-          imageUrl="/images/Rawana-logo.png"
+      <div className="w-20 sm:w-36">
+        <Image
+          imageUrl="/images/Rawana-white.svg"
           className="w-full h-full object-cover"
           alt="Trent Logo"
-        /> */}
-        RAWANA
+        />
       </div>
       <span className="text-xs text-white font-medium">Travel.hotel.easy</span>
     </Link>
